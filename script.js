fab32348f381d945986a398f674d8df64225e9dc
Abraham Mejia <aimejia4@gmail.com>
2023-08-29 02:37:23 -0500

    New assignment completed

---
+++ script.js
@@ -0,0 +1,23 @@
+// Exercises & Committing to GitHub:
+// Set up a new JavaScript file dedicated to practicing with objects.
+// Construct an object representing a book, with properties such as title, author, publication year, and genre.
+
+let book = {
+  title,
+  author: 'James Clear',
+  year: 2019,
+  genre
+};
+
+// Implement functions to add, modify, and delete properties from this book object.
+book.title = "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones";
+book.year = 2018;
+book.genre = 'advice';
+
+delete book.genre
+
+
+// To ensure you've mastered the topic, access and display the book's properties.
+// Once satisfied, commit your JavaScript file to a GitHub repository. This will not only serve as a record but will also allow you to share your progress and findings with peers.
+
+
